74e5ac15aac6338a08448339366d9f31067eff0b
Alexandra <aburbulea@gmail.com>
2024-08-21 11:58:57 +0300

    Add weapon models, create migrations

---
+++ Dtos/Weapon/GetWeaponResponseDto.cs
@@ -0,0 +1,8 @@
+namespace dotnet_rpg.Dtos.Weapon
+{
+    public class GetWeaponResponseDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Damage { get; set; }
+    }
+}
