6ba9787021ff8468a64c5f853ad829db780f4c37
Alexandra <aburbulea@gmail.com>
2024-08-21 12:53:27 +0300

    Add weapon attack model and update related models

--- Dtos/Character/GetCharacterResponseDto.cs
+++ Dtos/Character/GetCharacterResponseDto.cs
@@ -1,3 +1,5 @@
+using dotnet_rpg.Dtos.Skill;
+using dotnet_rpg.Dtos.Weapon;
 using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Dtos.Character
@@ -11,5 +13,10 @@ namespace dotnet_rpg.Dtos.Character
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public GetWeaponResponseDto? Weapon { get; set; }
+        public List<GetSkillResponseDto>? Skills { get; set; }
+        public int Fights { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
     }
 }
