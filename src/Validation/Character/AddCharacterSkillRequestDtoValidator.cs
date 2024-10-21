e2612c8eb62b314ab8b431baabd0c19d49c988e0
STICH\aburb <aburbulea@gmail.com>
2024-10-21 11:52:34 +0300

    Add fluent validation for AddCharacterSkillRequestDto, add covering unit tests

---
+++ src/Validation/Character/AddCharacterSkillRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_rpg.Dtos.Character;
+using FluentValidation;
+
+namespace dotnet_rpg.Validation.Character
+{
+    public class AddCharacterSkillRequestDtoValidator : AbstractValidator<AddCharacterSkillRequestDto>
+    {
+        public AddCharacterSkillRequestDtoValidator()
+        {
+            RuleFor(x => x.CharacterId)
+                .GreaterThan(0).WithMessage("Character Id must be greater than 0.");
+
+            RuleFor(x => x.SkillId)
+                .GreaterThan(0).WithMessage("Skill id must be greater than 0.");
+        }
+    }
+}
---
+++ test/Validation/Character/AddCharacterSkillRequestDtoValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Validation.Character;
+using FluentValidation.TestHelper;
+
+namespace dotnet_rpg.Tests.Validation.Character
+{
+    public class AddCharacterSkillRequestDtoValidatorTests
+    {
+        private readonly AddCharacterSkillRequestDtoValidator validator;
+
+        public AddCharacterSkillRequestDtoValidatorTests()
+        {
+            validator = new AddCharacterSkillRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Should_PassValidation_When_AllFieldsAreValid()
+        {
+            //Arrange
+            var dto = new AddCharacterSkillRequestDto
+            {
+                CharacterId = 1,
+                SkillId = 2
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void Should_FailValidation_When_IdsAreInvalid(int value)
+        {
+            //Arrange
+            var dto = new AddCharacterSkillRequestDto
+            {
+                CharacterId = value,
+                SkillId = value
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.CharacterId);
+            result.ShouldHaveValidationErrorFor(x => x.SkillId);
+        }
+    }
+}
