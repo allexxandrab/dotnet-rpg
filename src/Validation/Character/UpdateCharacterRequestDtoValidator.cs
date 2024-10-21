1fca234fb5347968a14e182a630951e32a19322c
STICH\aburb <aburbulea@gmail.com>
2024-10-21 12:15:25 +0300

    Add fluent validation for UpdateCharacterRequestDto, add covering unit tests

---
+++ src/Validation/Character/UpdateCharacterRequestDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using dotnet_rpg.Dtos.Character;
+using FluentValidation;
+using static dotnet_rpg.Constants;
+
+namespace dotnet_rpg.Validation.Character
+{
+    public class UpdateCharacterRequestDtoValidator : AbstractValidator<UpdateCharacterRequestDto>
+    {
+        public UpdateCharacterRequestDtoValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Character Id must be greater than 0.");
+
+            RuleFor(x => x.Name)
+              .Length(ValidationLimits.NameMinValue, ValidationLimits.NameMaxValue).WithMessage("Character name should be between 2 and 30 characters.");
+
+            RuleFor(x => x.HitPoints)
+                .GreaterThanOrEqualTo(ValidationLimits.HitPointsMinValue).WithMessage("Minimum hit points value is 1.")
+                .LessThanOrEqualTo(ValidationLimits.HitPointsMaxValue).WithMessage("Maximum hit points value is 100.");
+
+            RuleFor(x => x.Strength).ValidateStat("Strength");
+            RuleFor(x => x.Defense).ValidateStat("Defense");
+            RuleFor(x => x.Intelligence).ValidateStat("Intelligence");
+
+            RuleFor(x => x.Class)
+                .IsInEnum().WithMessage("There is no such class option.");
+        }
+    }
+}
---
+++ test/Validation/Character/UpdateCharacterRequestDtoValidatorTests.cs
@@ -0,0 +1,123 @@
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Models;
+using dotnet_rpg.Validation.Character;
+using FluentValidation.TestHelper;
+using static dotnet_rpg.Constants;
+
+namespace dotnet_rpg.Tests.Validation.Character
+{
+    public class UpdateCharacterRequestDtoValidatorTests
+    {
+        private readonly UpdateCharacterRequestDtoValidator validator;
+
+        public UpdateCharacterRequestDtoValidatorTests()
+        {
+            validator = new UpdateCharacterRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Should_PassValidation_When_AllFieldsAreValid()
+        {
+            //Arrange
+            var dto = new UpdateCharacterRequestDto
+            {
+                Id = 1,
+                Name = "Yoda",
+                HitPoints = 50,
+                Strength = 5,
+                Defense = 5,
+                Intelligence = 5,
+                Class = RpgClass.Knight
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Should_FailValidation_When_IdIsEmpty()
+        {
+            //Arrange
+            var dto = new UpdateCharacterRequestDto
+            {
+                Id = 0,
+                Name = "Yoda",
+                HitPoints = 50,
+                Strength = 5,
+                Defense = 5,
+                Intelligence = 5,
+                Class = RpgClass.Knight
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.Id);
+            result.ShouldNotHaveValidationErrorFor(x => x.HitPoints);
+            result.ShouldNotHaveValidationErrorFor(x => x.Strength);
+            result.ShouldNotHaveValidationErrorFor(x => x.Defense);
+            result.ShouldNotHaveValidationErrorFor(x => x.Intelligence);
+            result.ShouldNotHaveValidationErrorFor(x => x.Class);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Theory]
+        [InlineData(Test.InvalidRpgClasses.Notamage)]
+        [InlineData(Test.InvalidRpgClasses.Knnight)]
+        public void Should_FailValidation_When_ClassInvalid(Enum value)
+        {
+            //Arrange
+            var dto = new UpdateCharacterRequestDto
+            {
+                Name = "Yoda",
+                HitPoints = 50,
+                Strength = 5,
+                Defense = 5,
+                Intelligence = 5,
+                Class = (RpgClass)value
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.Class);
+            result.ShouldNotHaveValidationErrorFor(x => x.HitPoints);
+            result.ShouldNotHaveValidationErrorFor(x => x.Strength);
+            result.ShouldNotHaveValidationErrorFor(x => x.Defense);
+            result.ShouldNotHaveValidationErrorFor(x => x.Intelligence);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Theory]
+        [InlineData(0, ValidationLimits.HitPointsMinValue - 1)]
+        [InlineData(ValidationLimits.IntMaxValue + 1, ValidationLimits.HitPointsMaxValue + 1)]
+        public void Should_FailValidation_When_IntegerProperties_AreInvalid(int value, int hitPointsValue)
+        {
+            //Arrange
+            var dto = new UpdateCharacterRequestDto
+            {
+                Name = "Yoda",
+                HitPoints = hitPointsValue,
+                Strength = value,
+                Defense = value,
+                Intelligence = value,
+                Class = RpgClass.Knight
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+            result.ShouldHaveValidationErrorFor(x => x.HitPoints);
+            result.ShouldHaveValidationErrorFor(x => x.Strength);
+            result.ShouldHaveValidationErrorFor(x => x.Defense);
+            result.ShouldHaveValidationErrorFor(x => x.Intelligence);
+        }
+    }
+}
