c9aa03ff19bfaefc0ca89318f4d34b051164b91e
STICH\aburb <aburbulea@gmail.com>
2024-10-28 12:30:35 +0200

    Add fluent validation for WeaponAttackRequestDto, add covering unit tests

---
+++ src/Validation/Fight/WeaponAttackRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using FluentValidation;
+
+namespace dotnet_rpg.Validation.Fight
+{
+    public class WeaponAttackRequestDtoValidator : AbstractValidator<WeaponAttackRequestDto>
+    {
+        public WeaponAttackRequestDtoValidator()
+        {
+            RuleFor(x => x.AttackerId)
+                .GreaterThan(0).WithMessage("Attacker Id must be greater than 0.");
+
+            RuleFor(x => x.OpponentId)
+                .GreaterThan(0).WithMessage("Opponent Id must be greater than 0.");
+        }
+    }
+}
---
+++ test/Validation/Fight/WeaponAttackRequestDtoValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Validation.Fight;
+using FluentValidation.TestHelper;
+
+namespace dotnet_rpg.Tests.Validation.Fight
+{
+    public class WeaponAttackRequestDtoValidatorTests
+    {
+        private readonly WeaponAttackRequestDtoValidator validator;
+
+        public WeaponAttackRequestDtoValidatorTests()
+        {
+            validator = new WeaponAttackRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Should_PassValidation_When_AllFieldsAreValid()
+        {
+            //Arrange
+            var dto = new WeaponAttackRequestDto
+            {
+                AttackerId = 3,
+                OpponentId = 2
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(2, 0)]
+        [InlineData(0, 4)]
+
+        public void Should_FailValidation_When_FieldIsInvalid(int attackerId, int opponentId)
+        {
+            //Arrange
+            var dto = new WeaponAttackRequestDto
+            {
+                AttackerId = attackerId,
+                OpponentId = opponentId
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
