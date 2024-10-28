f7607abc1b0b86b66ca0ccecdcda4849955e2560
STICH\aburb <aburbulea@gmail.com>
2024-10-28 12:12:32 +0200

    Add FightRequestDtoValidator and covering unit tests

---
+++ src/Validation/Fight/FightRequestDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using FluentValidation;
+
+namespace dotnet_rpg.Validation.Fight
+{
+    public class FightRequestDtoValidator : AbstractValidator<FightRequestDto>
+    {
+        public FightRequestDtoValidator() 
+        {
+            RuleFor(x => x.CharacterIds)
+                .NotEmpty().WithMessage("Character Ids are required.")
+                .Must(ids => ids.All(id => id > 0)).WithMessage("All Character Ids must be greater than 0.")
+                .Must(ids => ids.Count >= 2).WithMessage("At least two characters are required for a fight.");
+        }
+    }
+}
---
+++ test/Validation/Fight/FightRequestDtoValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Validation.Fight;
+using FluentValidation.TestHelper;
+
+namespace dotnet_rpg.Tests.Validation.Fight
+{
+    public class FightRequestDtoValidatorTests
+    {
+        private readonly FightRequestDtoValidator validator;
+
+        public FightRequestDtoValidatorTests()
+        {
+            validator = new FightRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Should_PassValidation_When_AllFieldsAreValid()
+        {
+            //Arrange
+            var dto = new FightRequestDto
+            {
+                CharacterIds = { 2, 3 }
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+
+        [Theory]
+        [InlineData(new int[] {})]
+        [InlineData(new int[] { 0, 1})]
+        [InlineData(new int[] { -6, 5 })]
+        [InlineData(new int[] { 5 })]
+        public void Should_FailValidation_When_NoIdsaArePassed(int[] characterIds)
+        {
+            //Arrange
+            var dto = new FightRequestDto
+            {
+                CharacterIds = characterIds.ToList()
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.CharacterIds);
+        }
+    }
+}
