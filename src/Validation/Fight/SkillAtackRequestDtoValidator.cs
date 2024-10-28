ab0f2243ebf68f09158d974200a7ea9b04e3cc43
STICH\aburb <aburbulea@gmail.com>
2024-10-28 12:25:50 +0200

    Add fluent validation for SkillAtackRequestDto, add covering unit tests

---
+++ src/Validation/Fight/SkillAtackRequestDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using FluentValidation;
+
+namespace dotnet_rpg.Validation.Fight
+{
+    public class SkillAtackRequestDtoValidator : AbstractValidator<SkillAtackRequestDto>
+    {
+        public SkillAtackRequestDtoValidator()
+        {
+            RuleFor(x => x.AttackerId)
+                .GreaterThan(0).WithMessage("Attacker Id must be greater than 0.");
+
+            RuleFor(x => x.OpponentId)
+                .GreaterThan(0).WithMessage("Opponent Id must be greater than 0.");
+
+            RuleFor(x => x.SkillId)
+               .GreaterThan(0).WithMessage("Skill Id must be greater than 0.");
+        }
+    }
+}
---
+++ test/Validation/Fight/SkillAtackRequestDtoValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Validation.Fight;
+using FluentValidation.TestHelper;
+
+namespace dotnet_rpg.Tests.Validation.Fight
+{
+    public class SkillAtackRequestDtoValidatorTests
+    {
+        private readonly SkillAtackRequestDtoValidator validator;
+
+        public SkillAtackRequestDtoValidatorTests()
+        {
+            validator = new SkillAtackRequestDtoValidator();
+        }
+
+        [Fact]
+        public void Should_PassValidation_When_AllFieldsAreValid()
+        {
+            //Arrange
+            var dto = new SkillAtackRequestDto
+            {
+                AttackerId = 3,
+                OpponentId = 2, 
+                SkillId = 3
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(2, 0, 1)]
+        [InlineData(1, 2, 0)]
+        [InlineData(0, 6, 1)]
+
+        public void Should_FailValidation_When_FieldIsInvalid(int attackerId, int opponentId, int skillId)
+        {
+            //Arrange
+            var dto = new SkillAtackRequestDto
+            {
+                AttackerId = attackerId,
+                OpponentId = opponentId,
+                SkillId = skillId
+            };
+
+            //Act
+            var result = validator.TestValidate(dto);
+
+            //Assert
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
