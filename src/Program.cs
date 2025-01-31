9a94c17adf3c277c7ae785a56458daf756f0cc0c
STICH\aburb <aburbulea@gmail.com>
2025-01-31 17:16:22 +0200

    Data preparation for docker setup

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,9 @@ using dotnet_rpg.Repositories.Implementation;
 using dotnet_rpg.Services;
 using dotnet_rpg.Services.Implementation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
@@ -15,7 +17,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{ 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -57,6 +63,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    try { 
+        dbContext.Database.CanConnect();
+        dbContext.Database.Migrate(); 
+    } catch (SqlException e)
+    {
+        Console.WriteLine(e.Message);
+    } 
 }
 
 app.UseHttpsRedirection();
